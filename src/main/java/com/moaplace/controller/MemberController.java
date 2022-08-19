ad140cf7f2648e648f519e02b49bcbafd3a5f19f
RadiantK <pps8853@gmail.com>
2022-08-19 14:34:20 +0900

    오타 수정 및 프로젝트 기본 설정 테스트

--- src/main/java/com/moaplace/controller/MemberController.java
+++ src/main/java/com/moaplace/controller/MemberController.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class MemberController {
 
-	@PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Map<String, Object> login(@RequestParam String id, int age) {
+	@PostMapping(value = "/info",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> postTest(
+			@RequestBody Map<String, Object> user) {
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", user.get("id"));
+		map.put("age", user.get("age"));
+		
+		return map;
+	}
+	
+	@GetMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> login(
+			@RequestParam String id,@RequestParam int age) {
 		
 		Map<String, Object> map = new HashMap<>();
 		map.put("id", id);
