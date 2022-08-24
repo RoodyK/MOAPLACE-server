f02b4cff2702165a7504cd4dce51669aa4528e15
RadiantK <pps8853@gmail.com>
2022-08-24 12:27:27 +0900

    로그인 및 회원가입 이메일인증 처리

--- src/main/java/com/moaplace/mapper/MemberMapper.java
+++ src/main/java/com/moaplace/mapper/MemberMapper.java
@@ -3,15 +3,18 @@ package com.moaplace.mapper;
 import java.util.List;
 
 import com.moaplace.dto.MemberJoinRequestDTO;
+import com.moaplace.dto.MemberLoginRequestDTO;
+import com.moaplace.dto.MemberLoginResponseDTO;
 import com.moaplace.vo.MemberVO;
 
 public interface MemberMapper {
 
 	public String findById();
 	
-	public String findByPassword();
+	// 로그인용 비밀번호 가져오기
+	public String findByPassword(String id);
 	
-	public String login();
+	public MemberLoginResponseDTO login(MemberLoginRequestDTO dto);
 	
 	public List<MemberVO> selectAll();
 	
