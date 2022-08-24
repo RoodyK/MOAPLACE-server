abadde52cd1788ca7d3c977105a3db32583a3c4b
RadiantK <pps8853@gmail.com>
2022-08-24 12:27:42 +0900

    이메일 인증 및 로그인 처리

--- src/main/java/com/moaplace/service/MailSendService.java
+++ src/main/java/com/moaplace/service/MailSendService.java
@@ -39,12 +39,13 @@ public class MailSendService {
 		String toMail = email;
 		// 메일 제목
 		String title ="회원가입 인증 이메일 입니다.";
+		// 이메일 내용 삽입
 		String content = 
 				"<h1>MOA PLACE를 방문해주셔서 감사합니다.</h1>" + 
 				"<br/><br/>" + 
 				"인증번호는 " + n + " 입니다." + 
 				"<br/>" + 
-				"해당 인증번호를 인증번호 확인란에 기입하여 주세요."; // 이메일 내용 삽입
+				"해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
 		mailSend(setFrom, toMail, title, content);
 		
 		return Integer.toString(n);
@@ -57,10 +58,12 @@ public class MailSendService {
 		// 스프링에서 제공하는 mailAPI
 		MimeMessage message = mailSender.createMimeMessage();
 		
-		
 		try {
+			// 단순한 텍스트 형태 전송
+			// MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
 			// true 매개값을 전달하면 multipart 형식의 메시지 전달이 가능, 문자 인코딩 설정도 가능하다.
 			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+			helper.setSubject("MOA PLACE 회원가입 인증번호 입니다.");
 			helper.setFrom(setFrom);
 			helper.setTo(toMail);
 			helper.setSubject(title);
