b319d1e5e209baa628ce6095fffe9101b83e6aa9
walnutB <101103326+walnutB@users.noreply.github.com>
2022-08-25 01:36:32 +0900

    공연정보 조회 기능 구현

--- src/main/java/com/moaplace/controller/admin/show/ShowInsertController.java
+++ src/main/java/com/moaplace/controller/admin/show/ShowInsertController.java
@@ -2,16 +2,22 @@ package com.moaplace.controller.admin.show;
 
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.moaplace.dto.admin.show.MapperDetailDTO;
+import com.moaplace.dto.admin.show.ShowDetailViewDTO;
 import com.moaplace.dto.admin.show.ShowInsertRequestDTO;
+import com.moaplace.dto.admin.show.ShowListDTO;
 import com.moaplace.service.ShowService;
 
 import lombok.extern.log4j.Log4j;
@@ -43,4 +49,28 @@ public class ShowInsertController {
 		return map;
 		
 	}
+	@GetMapping(value = "/list")
+	public HashMap<String, Object> list(){
+		
+		List<ShowListDTO> list=service.showList();
+		HashMap<String, Object> map=new HashMap<String, Object>();
+		map.put("list",list);
+		if(list.size() < 1) {
+			return null;
+		}
+		return map;
+	}
+	
+	@GetMapping( value = "/detail/{showNum}")
+	public HashMap<String, Object> showDetail(
+			@PathVariable("showNum") int showNum){
+		
+		log.info("요청 들어옴");
+		ShowDetailViewDTO dto= service.showDetail(showNum);
+		HashMap<String, Object> map=new HashMap<String, Object>();
+			map.put("list",dto);
+		return map;
+	}
+	
+	
 }
---
+++ src/main/java/com/moaplace/util/PageUtil.java
@@ -0,0 +1,44 @@
+package com.moaplace.util;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Data
+@ToString
+public class PageUtil {
+	private int pageNum; // 페이지번호
+	private int startRow; // 시작행번호 
+	private int endRow; // 끝행번호
+	private int totalPageCount; // 전체페이지 개수
+	private int startPageNum; // 시작페이지 번호
+	private int endPageNum; // 끝페이지 번호
+	private int rowBlockCount; // 한페이지에 보여질 글의 개수
+	private int pageBlockCount; // 한페이지에 보여질 페이지의 개수
+	private int totalRowCount; // 전체 글의 개수
+	
+	/*
+	 * @param pageNum
+	 * @param rowBlockCount 
+	 * @param pageBlockCount 
+	 * @param totalRowCount 
+	 */
+	
+	public PageUtil(int pageNum, int rowBlockCount, int pageBlockCount, int totalRowCount) {
+		this.pageNum = pageNum;
+		this.rowBlockCount = rowBlockCount;
+		this.pageBlockCount = pageBlockCount;
+		this.totalRowCount = totalRowCount;
+
+		startRow = (pageNum-1)*rowBlockCount+1;
+		endRow = startRow + rowBlockCount-1;
+		totalPageCount = (int)Math.ceil(totalRowCount/(double)rowBlockCount);
+		startPageNum = (pageNum-1)/pageBlockCount*pageBlockCount+1;
+		endPageNum = startPageNum + pageBlockCount-1;
+		
+		if(totalPageCount<endPageNum) {
+			endPageNum = totalPageCount;
+		}
+	}
+}
