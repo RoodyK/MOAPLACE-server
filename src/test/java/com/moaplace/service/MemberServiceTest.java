645b75b40ee31291316000f56e612bd715e11aae
RadiantK <pps8853@gmail.com>
2022-08-27 01:21:05 +0900

    회원 아이디/비밀번호 찾기 테스트

--- src/test/java/com/moaplace/service/MemberServiceTest.java
+++ src/test/java/com/moaplace/service/MemberServiceTest.java
@@ -1,8 +1,11 @@
 package com.moaplace.service;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,21 +32,24 @@ public class MemberServiceTest {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
+	// 패스워드 인코더 테스트
 	@Test
 	public void passwordEncoderTest() {
-		
+		log.info(passwordEncoder.encode("123123"));
 		log.info(passwordEncoder.matches("!temp1234", "$2a$10$X5p.Ib1B0X22WGhh78DxA.x/NT.FaW6vKdr4VhYFF.neXOWCgB4o."));
 	}
 	
+	// 회원정보 테스트
 	@Test
 	public void selectListTest() {
 		List<MemberVO> vo = memberService.selectAll();
 		for( MemberVO m : vo) {
 			System.out.println(m);
 		}
-		log.info(passwordEncoder.encode("123123"));
+
 	}
 	
+	// 회원가입 테스트
 	@Test
 	public void insertTest() {
 		MemberJoinRequestDTO dto = new MemberJoinRequestDTO();
@@ -59,5 +65,17 @@ public class MemberServiceTest {
 		
 		int n = memberService.join(dto);
 		assertEquals(1, n);
+	}
+	
+	// 아이디 찾기 테스트
+	@Test
+	public void findByIdTest() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "관리자");
+		map.put("email", "admin@gmail.com");
+		
+		String id = memberService.findById(map);
+		
+		assertNotNull(id);
 	}
 }
