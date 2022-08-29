417318bd8fd9c6b351359acedf5bfe2b1dba1829
colmaa <neoncoalmine@gmail.com>
2022-08-29 21:39:40 +0900

    favorite  테이블 VO 생성 및 회원별 관심공연 조회를 위한 favorite  DTO,Mapper,Service 생성

---
+++ src/main/java/com/moaplace/dto/MyFavoriteDTO.java
@@ -0,0 +1,20 @@
+package com.moaplace.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyFavoriteDTO {
+	private int rnum;
+	private int member_num;
+	private String show_name;
+	private Date show_start;
+	private Date show_end;
+	private String genre_category;
+	private String show_thumbnail;
+}
