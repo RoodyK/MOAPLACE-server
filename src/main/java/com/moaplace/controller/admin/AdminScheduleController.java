6896dabed9df049ea70563c4209188c14cabba28
walnutB <101103326+walnutB@users.noreply.github.com>
2022-08-30 12:21:43 +0900

    공연일정 페이징처리

--- src/main/java/com/moaplace/controller/admin/AdminScheduleController.java
+++ src/main/java/com/moaplace/controller/admin/AdminScheduleController.java
@@ -14,11 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.moaplace.dto.admin.show.ScheduleInsertRequestDTO;
-import com.moaplace.dto.admin.show.ShowListDTO;
+import com.moaplace.dto.admin.show.ScheduleListDTO;
 import com.moaplace.service.AdminTicketService;
-import com.moaplace.service.ShowService;
 import com.moaplace.util.PageUtil;
-
 import lombok.extern.log4j.Log4j;
 
 
@@ -52,21 +50,29 @@ public class AdminScheduleController {
 		return service.insertSchedule(dto);
 	}
 	@GetMapping(
-			value = {"/list"},
+			value = {"/list",
+					"/list/{pageNum}"},
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	
-	public HashMap<String, Object> list(){		
+	public HashMap<String, Object> list(
+			@PathVariable ( required = false ) Integer pageNum){		
 		
-		// db로 조회할 정보를 해시맵에 담기 (시작/끝행,검색조건)
-//		HashMap<String, Object> sList = new HashMap<String, Object>();
-//		sList.put( "startRow", pu.getStartRow());
-//		sList.put( "endRow", pu.getEndRow());
+		//페이지번호가 null값일 때(페이지에 처음 가거나 새로고침했을때) 기본값으로 넣기
+		if(pageNum==null)pageNum=1;
+		
+		// 페이징처리하는 클래스 유틸 받아서 페이징 데이터 생성
+		PageUtil pu = new PageUtil(pageNum,5,5,service.sheduleCount());
+		
+		 //db로 조회할 정보를 해시맵에 담기 (시작/끝행,검색조건)
+		HashMap<String, Object> sList = new HashMap<String, Object>();
+		sList.put( "startRow", pu.getStartRow());
+		sList.put( "endRow", pu.getEndRow());
 //		sList.put( "showCheck", status);
 //		sList.put( "field", field);
 //		sList.put( "search", search);
 //
-//		// 공연목록 조회해서 받아올 DTO 새로 만들어서 매퍼 리턴 정보 리스트에 담기
-//		List<ShowListDTO> list=service.showList(sList);
+		// 공연목록 조회해서 받아올 DTO 새로 만들어서 매퍼 리턴 정보 리스트에 담기
+		List<ScheduleListDTO> list=service.sheduleList(sList);
 //		
 //		// 검색어가 있고, 공연상태별 모아보기가 선택됐을 때 다시 조회된 행 번호 카운트해서 페이징처리 유틸에 새로 덮어씌우기 
 //		if(search!=null || !status.equals("all")) {
@@ -74,13 +80,17 @@ public class AdminScheduleController {
 //					pageNum,5,5,service.currentListRow(sList));
 //		};
 		
+		log.info(pu.getEndPageNum());
+		
 		// 해시맵에 조회된 공연목록, 페이지번호, 페이징유틸 정보 담아서 클라이언트로 보내기
 		// 해시맵을 produces = MediaType.APPLICATION_JSON_VALUE 으로 제이슨형태로 변환해서 전송
 //		if(search==null)search="";
 		HashMap<String, Object> map=new HashMap<String, Object>();
-		map.put( "list", service.sheduleList());
-//		map.put( "pageNum", pageNum);
-//		map.put( "pageInfo", pu);
+		map.put( "list",list);
+		map.put( "pageNum", pageNum);
+		map.put( "startPageNum", pu.getStartPageNum());
+		map.put( "endPageNum", pu.getEndPageNum());
+		map.put( "totalPageCount", pu.getTotalPageCount());
 //		map.put("status", status);
 //		map.put("selectField", field);
 //		map.put("search", search);
