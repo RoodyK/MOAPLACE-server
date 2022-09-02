c7a16c856907ac81bd1215bb9dfc261d266b28ed
RadiantK <pps8853@gmail.com>
2022-09-02 10:16:46 +0900

    jwt 만료시간 30분적용, 만료시간 인터셉터 설정

--- src/main/java/com/moaplace/authentication/BearerTokenAuthenticationFilter.java
+++ src/main/java/com/moaplace/authentication/BearerTokenAuthenticationFilter.java
@@ -3,31 +3,42 @@ package com.moaplace.authentication;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.moaplace.exception.AuthenticationException;
+import com.moaplace.service.JWTService;
 
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class BearerTokenAuthenticationFilter implements HandlerInterceptor {
 	
+	@Autowired
+	private JWTService service;
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// 헤더에서 토큰 추출
 		
-		String token = request.getHeader("Authorization");
+		String token = request.getHeader("Authorization");				
 		
-//		String jwtToken = token.split(" ")[1];
+		log.info("======================================");
 		log.info("토큰 : " + token);
-//		
+		log.info("======================================");
+		
 //		if(token == null) {
 //			throw new AuthenticationException();
 //		}
-//		
+
 //		// 토큰 만료되었는지 확인 true 아직 만료시간 남음 false 만료
-//		if(!service.validateToken(token)) {
-//			throw new AuthenticationException(); 
-//		}
+		if(token != null) {
+			String jwtToken = token.split(" ")[1];
+			if(!service.validateToken(jwtToken)) {
+				throw new AuthenticationException(); 
+			}
+		}
 		
 //		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		return true;
